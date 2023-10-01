243e06247eb2e7d196fb620b12b77cff621b1335
Juan Ortega <js.ortegar12@uniandes.edu.co>
2023-09-30 23:08:51 -0500

    dar info cliente, consumo, habitacion y hotel

--- demo/src/main/java/com/example/demo/repositorio/ClienteRepository.java
+++ demo/src/main/java/com/example/demo/repositorio/ClienteRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repositorio;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.modelo.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente,  Integer> {
     
+    @Query(value = "SELECT * FROM Clientes", nativeQuery = true)
+    Collection<Cliente> darClientes();
 }
--- demo/src/main/java/com/example/demo/repositorio/ConsumoRepository.java
+++ demo/src/main/java/com/example/demo/repositorio/ConsumoRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repositorio;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.modelo.Consumo;
 
 public interface ConsumoRepository extends JpaRepository<Consumo,  Integer>{
-    
+
+    @Query(value = "SELECT * FROM Consumo", nativeQuery = true)
+    Collection<Consumo> darConsumos();
 }
