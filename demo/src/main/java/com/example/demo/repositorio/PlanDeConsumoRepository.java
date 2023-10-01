113dba137789d3543ba711e7ab0afc093669f0dc
Juan Ortega <js.ortegar12@uniandes.edu.co>
2023-10-01 02:02:49 -0500

    plan de consumo

--- demo/src/main/java/com/example/demo/repositorio/PlanDeConsumoRepository.java
+++ demo/src/main/java/com/example/demo/repositorio/PlanDeConsumoRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.modelo.PlanDeConsumo;
+
 
 public interface PlanDeConsumoRepository extends JpaRepository<PlanDeConsumo, Integer>  {
     
